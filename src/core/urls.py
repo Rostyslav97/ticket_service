32649610b3332269f61ea48510fd6f3398b11ae3
Rostyslav <malanyukrostik@gmail.com>
2022-05-06 13:29:48 +0300

    DestroyEventAPI

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import ListEventAPI, RetrieveEventAPI, CreateEventAPI
+from core.views import ListEventAPI, RetrieveEventAPI, CreateEventAPI, DestroyEventAPI
 
 
 urlpatterns = [
     path('events/', ListEventAPI.as_view()),
     path('events/<int:id>/', RetrieveEventAPI.as_view()),
-    path('events/create/', CreateEventAPI.as_view())
+    path('events/create/', CreateEventAPI.as_view()),
+    path('events/destroy/<int:id>/', DestroyEventAPI.as_view())
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,19 +1,25 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView
 from core.models import Event
-from core.serializers import ListRetrieveEventSerializer, CreateEventSerializer
+from core.serializers import ListRetrieveDestroyEventSerializer, CreateEventSerializer
 
 
 class ListEventAPI(ListAPIView):
     queryset = Event.objects.all()
-    serializer_class = ListRetrieveEventSerializer
+    serializer_class = ListRetrieveDestroyEventSerializer
 
 
 class RetrieveEventAPI(RetrieveAPIView):
     queryset = Event.objects.all()
-    serializer_class = ListRetrieveEventSerializer
+    serializer_class = ListRetrieveDestroyEventSerializer
     lookup_field = "id"
 
 
 class CreateEventAPI(CreateAPIView):
     queryset = Event.objects.all()
     serializer_class = CreateEventSerializer
+
+
+class DestroyEventAPI(DestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = ListRetrieveDestroyEventSerializer
+    lookup_field = "id"
