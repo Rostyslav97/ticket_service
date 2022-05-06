1aa300c0122e5976ebe8d7446f85f5c128eb76e5
Rostyslav <malanyukrostik@gmail.com>
2022-05-06 12:19:45 +0300

    edit ListEventSerializer

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -3,6 +3,9 @@ from core.models import Event
 
 
 class ListEventSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    ground = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    currency = serializers.SlugRelatedField(slug_field="name", read_only=True)
     class Meta:
         model = Event
         exclude = ("id", )
